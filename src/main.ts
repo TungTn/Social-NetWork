57cf2846c76363bf2a489c8b33843836c7a39a3f
Tran Tung <tran.tung1311@gmail.com>
2021-12-06 16:33:00 +0700

    Commit file code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
-import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
+const base = axios.create({
+    baseURL: "https://api.jsonbin.io/b/5f20829bc58dc34bf5dca275",
+});
+Vue.prototype.$http = base;
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
-
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    render: (h) => h(App),
 }).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/vn/cherry-blossom-test-v3/'
+    : '/'
+}
